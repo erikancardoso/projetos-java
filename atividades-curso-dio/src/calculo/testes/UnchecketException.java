19340294ffdf8f2b60c9b40456b23acd6327ccf1
erikancardoso <erikancardoso@outlook.com>
2022-08-07 16:20:24 -0400

    testes try catch

--- atividades-curso-dio/src/calculo/testes/UnchecketException.java
+++ atividades-curso-dio/src/calculo/testes/UnchecketException.java
@@ -6,21 +6,25 @@ import javax.swing.*;
 public class UnchecketException {
     //crie uma divisão de dois valores inteiros e peça para o usuário informar os valores
     public static void main(String[] args) {
-        String a = JOptionPane.showInputDialog("Numerador"); //mostra um imput na tela em caixa e armazena a String
-        String b = JOptionPane.showInputDialog("Denominador");
-        try {
-            int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b)); // parsando string para tipo inteiro
-            System.out.println(resultado);
-        }catch (NumberFormatException e){
-            JOptionPane.showMessageDialog(null,"entrada invalida, informe um numero!"+e.getMessage());
-            //e.printStackTrace();
-        }catch (ArithmeticException e){
-            JOptionPane.showMessageDialog(null,"não é possível dividir um numero por "+e.getMessage());
-        } finally {
-            System.out.println("Chegou ao finally");
-        }
-        System.out.println("O codigo continua...");
 
+        boolean tenteNovamente = true;
+        do {
+                String a = JOptionPane.showInputDialog("Numerador"); //mostra um imput na tela em caixa e armazena a String
+                String b = JOptionPane.showInputDialog("Denominador");
+            try {
+                int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b)); // parsando string para tipo inteiro
+                System.out.println(resultado);
+                tenteNovamente = false;
+            }catch (NumberFormatException e){
+                JOptionPane.showMessageDialog(null,"entrada invalida, informe um numero!"+e.getMessage());
+                //e.printStackTrace();
+             }catch (ArithmeticException e){
+                JOptionPane.showMessageDialog(null,"não é possível dividir um numero por "+e.getMessage());
+            } finally {
+                System.out.println("Chegou ao finally");
+            }
+                System.out.println("O codigo continua...");
+        }while (tenteNovamente);
 
     }
 
